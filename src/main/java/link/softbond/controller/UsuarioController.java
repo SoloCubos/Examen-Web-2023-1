be412c45258c585ec49c6b67517d842932459ec6
SoloCubos <solanofernando35@gmail.com>
2023-06-26 12:31:06 -0500

    H05 Correcion en el postmapping

--- src/main/java/link/softbond/controller/UsuarioController.java
+++ src/main/java/link/softbond/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
     
-    @RequestMapping(path = "/registrar")
+    @PostMapping(path = "/registrar")
     public ResponseEntity<?> registrar(@RequestBody UsuarioDTO usuarioDTO){
         try {
             Usuario usuario = Usuario.builder()
